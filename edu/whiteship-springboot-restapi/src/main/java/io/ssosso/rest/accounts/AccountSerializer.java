82b6a808756cd4398b8a467958b811bcd3894c43
somyungsub <gkdldy5@naver.com>
2020-03-12 18:45:59 +0900

    Add : 40강 '출력값 제한하기' 강의 내용

---
+++ edu/whiteship-springboot-restapi/src/main/java/io/ssosso/rest/accounts/AccountSerializer.java
@@ -0,0 +1,21 @@
+package io.ssosso.rest.accounts;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.core.serializer.Serializer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class AccountSerializer extends JsonSerializer<Account> {
+
+  @Override
+  public void serialize(Account account, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    jsonGenerator.writeStartObject();
+
+    jsonGenerator.writeNumberField("id", account.getId());
+
+    jsonGenerator.writeEndObject();
+  }
+}
