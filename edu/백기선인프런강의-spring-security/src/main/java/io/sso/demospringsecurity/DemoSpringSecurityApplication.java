f06384a468afd4cd12097294f11679279f5919d8
somyungsub <gkdldy5@naver.com>
2020-03-15 12:03:32 +0900

    Add : 백기선님 'Spring Security' 강의 '스프링 시큐리티 커스터마이징 : PasswordEncoder'

--- edu/백기선인프런강의-spring-security/src/main/java/io/sso/demospringsecurity/DemoSpringSecurityApplication.java
+++ edu/백기선인프런강의-spring-security/src/main/java/io/sso/demospringsecurity/DemoSpringSecurityApplication.java
@@ -2,9 +2,27 @@ package io.sso.demospringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityApplication {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    // password 저장시 인코딩하여 저장 NoOp으로 저장하기
+//    return NoOpPasswordEncoder.getInstance();
+
+    /*
+      5버전 이후부터 는 NoOp의 디폴트전략이 변경됨, 다양한 인코딩 알고리즘을 사용할 수 있도록
+        - noop, pdkdf2, scrypt, sha256
+        - 디폴트 -> bcrypt
+     */
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(DemoSpringSecurityApplication.class, args);
   }
