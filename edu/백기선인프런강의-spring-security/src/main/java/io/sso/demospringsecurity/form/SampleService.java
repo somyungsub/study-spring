705e887b383e8ca8be2e0c03b8b74f99a15c0535
somyungsub <gkdldy5@naver.com>
2020-03-16 23:21:26 +0900

    Add : 백기선님 'Spring Security' 강의 'Thread Local'

--- edu/백기선인프런강의-spring-security/src/main/java/io/sso/demospringsecurity/form/SampleService.java
+++ edu/백기선인프런강의-spring-security/src/main/java/io/sso/demospringsecurity/form/SampleService.java
@@ -1,6 +1,7 @@
 package io.sso.demospringsecurity.form;
 
 
+import io.sso.demospringsecurity.account.Account;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,11 +12,15 @@ import java.util.Collection;
 @Service
 public class SampleService {
   public void dashboard() {
-    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    final Object principal = authentication.getPrincipal();
-    final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-    final Object credentials = authentication.getCredentials();
-    final boolean authenticated = authentication.isAuthenticated();
+//    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//    final Object principal = authentication.getPrincipal();
+//    final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//    final Object credentials = authentication.getCredentials();
+//    final boolean authenticated = authentication.isAuthenticated();
+
+    final Account account = AccountContext.getAccount();
+    System.out.println("====================");
+    System.out.println(account.getUsername());
 
   }
 }
