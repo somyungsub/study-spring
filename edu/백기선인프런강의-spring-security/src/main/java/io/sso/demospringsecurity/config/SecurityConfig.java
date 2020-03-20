6b2ffd9e266dbf635084f30d8b922b71b6048c72
somyungsub <gkdldy5@naver.com>
2020-03-20 12:57:24 +0900

    Add : 백기선님 'Spring Security' 강의 'AccessDecisionManager 2부'

--- edu/백기선인프런강의-spring-security/src/main/java/io/sso/demospringsecurity/config/SecurityConfig.java
+++ edu/백기선인프런강의-spring-security/src/main/java/io/sso/demospringsecurity/config/SecurityConfig.java
@@ -5,10 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 //@EnableWebSecurity
@@ -17,6 +28,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   AccountService accountService;
+
+  public AccessDecisionManager accessDecisionManager() {
+    // 정의를 하지 않으면 기본 생성 (AffirmativeBased)
+
+
+    // Role 정의
+    RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+    roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER"); // Role 하이라키
+
+    // Handler 셋팅
+    DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+    handler.setRoleHierarchy(roleHierarchy);
+
+    // WebExpressionVoter 세팅
+    WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+    webExpressionVoter.setExpressionHandler(handler);
+
+
+    List<AccessDecisionVoter<? extends Object>> webExpressionVoters = Arrays.asList(webExpressionVoter);
+    return new AffirmativeBased(webExpressionVoters);
+  }
+
+  public SecurityExpressionHandler expressionHandler() {
+    // 정의를 하지 않으면 기본 생성 (AffirmativeBased)
+
+
+    // Role 정의
+    RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+    roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER"); // Role 하이라키
+
+    // Handler 셋팅
+    DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+    handler.setRoleHierarchy(roleHierarchy);
+
+    return handler;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -27,19 +74,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        .anyRequest().permitAll();
 //
 //    http.formLogin();
-//    http.httpBasic();
 
+//    http.httpBasic();
     // 요청
     http
-            .antMatcher("/**")
             .authorizeRequests()
             .mvcMatchers("/", "/info", "/account/**").permitAll()  // /info 요청 모든 등급 허용, 로그인 x
             .mvcMatchers("/admin").hasRole("ADMIN") // /admin 요청 인증 + ADMIN 롤만 허용
+            .mvcMatchers("/user").hasRole("USER") // /user 요청 인증 + USer 롤만 허용
             .anyRequest().authenticated() // 나머지, 인증만 되면 허용
-          .and()
-        .formLogin()    // 로그인 , form, oauth, openid
-          .and()
-        .httpBasic()    // Http 기본 설정 정보 사용
+//            .accessDecisionManager(accessDecisionManager());
+            .expressionHandler(expressionHandler());
+
+    http.formLogin();    // 로그인 , form, oauth, openid
+    http.httpBasic()    // Http 기본 설정 정보 사용
     ;
   }
 
