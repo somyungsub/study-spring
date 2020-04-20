cfd4abfceb291b1c5943fb80fd88f2a31507851f
somyungsub <gkdldy5@naver.com>
2020-04-21 07:25:16 +0900

    Add : 더 자바 테스트 활용, JMeter 활용

--- springboot/the-java-test/src/main/java/com/example/demo/study/StudyRepository.java
+++ springboot/the-java-test/src/main/java/com/example/demo/study/StudyRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
-  Optional<Study> findById(String id);
+  Optional<Study> findById(Long id);
 }
