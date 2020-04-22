8ba2145c59decec2b54dd9d2f1d1bb67c6e80334
somyungsub <gkdldy5@naver.com>
2020-04-23 05:46:25 +0900

    Add : 더 자바 테스트 활용, Archunit 애노테이션

--- springboot/the-java-test/src/test/java/com/example/demo/study/ArchTests.java
+++ springboot/the-java-test/src/test/java/com/example/demo/study/ArchTests.java
@@ -1,15 +1,36 @@
 package com.example.demo.study;
 
 
+import com.example.demo.DemoApplication;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+@AnalyzeClasses(packagesOf = DemoApplication.class)
 class ArchTests {
+
+  @ArchTest
+  ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
+          .should().onlyBeAccessed().byClassesThat()
+          .resideInAnyPackage( "..member..", "..domain..");
+
+  @ArchTest
+  ArchRule memberPackageRule = noClasses().that().resideInAPackage("..domain..")
+          .should().accessClassesThat().resideInAPackage("..member..");
+
+  @ArchTest
+  ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
+          .should().accessClassesThat().resideInAPackage("..study..");
+
+  @ArchTest
+  ArchRule freeOfCycle = slices().matching("..demo.(*)..")
+          .should().beFreeOfCycles();
 
   @Test
   void packageDependencyTests() {
@@ -23,23 +44,18 @@ class ArchTests {
      * TODO 순환 참조 없어야 한다.
      */
 
-    ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
-            .should().onlyBeAccessed().byClassesThat()
-            .resideInAnyPackage( "..member..", "..domain..");
+
 //            .resideInAnyPackage("..study..", "..member..", "..domain..");
 //    domainPackageRule.check(classes);
 
-    ArchRule memberPackageRule = noClasses().that().resideInAPackage("..domain..")
-            .should().accessClassesThat().resideInAPackage("..member..");
+
 //    memberPackageRule.check(classes);
-    ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
-            .should().accessClassesThat().resideInAPackage("..study..");
+
 
 //    studyPackageRule.check(classes);
 
-    ArchRule freeOfCycle = slices().matching("..demo.(*)..")
-            .should().beFreeOfCycles();
-    freeOfCycle.check(classes);
+
+//    freeOfCycle.check(classes);
   }
 
 }
