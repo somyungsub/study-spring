1535fb322673d6a612a3eee193e8703ee892081e
somyungsub <gkdldy5@naver.com>
2020-04-24 07:40:24 +0900

    Add : Spring boot HATEOAS

---
+++ springboot/springboot-hateoas/src/main/java/io/ssosso/hateoas/HateoasApplication.java
@@ -0,0 +1,13 @@
+package io.ssosso.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasApplication.class, args);
+	}
+
+}
