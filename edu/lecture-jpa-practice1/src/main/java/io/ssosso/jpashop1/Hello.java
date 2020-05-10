50a1b2f16e46820b0984056c446c12a5035a7222
somyungsub <gkdldy5@naver.com>
2020-05-10 10:59:02 +0900

    Add : Project 세팅, 경로 정리

---
+++ edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/Hello.java
@@ -0,0 +1,10 @@
+package io.ssosso.jpashop1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+  private String name;
+}
---
+++ edu/lecture-jpa-practice1/src/test/java/io/ssosso/jpashop1/Jpashop1ApplicationTests.java
@@ -0,0 +1,14 @@
+package io.ssosso.jpashop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpashop1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+    System.out.println("true = " + true);
+  }
+
+}
