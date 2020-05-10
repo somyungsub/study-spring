d5efee05c3b576390c40fb846291357b8e8ede13
somyungsub <gkdldy5@naver.com>
2020-05-10 17:55:31 +0900

    Add : JPA 실전1, 엔티티 및 테이블 설계 / 생성 2

--- edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/domain/Address.java
+++ edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/domain/Address.java
@@ -6,12 +6,21 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
   private String city;
-
   private String street;
-
   private String zipcode;
+
+  // JPA 스펙상 디폴트 생성자 필요.. -> protected 까지 허용해주므로, public 대신 protected 로 해두자!
+  protected Address() {
+  }
+
+  // 불변 값을 위해 setter 제거하고, 생성자로 한번만 초기화 할 수 있도록 설계
+  public Address(String city, String street, String zipcode) {
+    this.city = city;
+    this.street = street;
+    this.zipcode = zipcode;
+  }
 }
