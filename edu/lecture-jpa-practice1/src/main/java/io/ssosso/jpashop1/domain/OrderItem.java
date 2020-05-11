287eb6a33ee17a636016ac9b4903a577f929c4d5
somyungsub <gkdldy5@naver.com>
2020-05-12 02:53:22 +0900

    Add : JPA 실전1, 주문,주문상품 서비스

--- edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/domain/OrderItem.java
+++ edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package io.ssosso.jpashop1.domain;
 
 import io.ssosso.jpashop1.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
   @Id
