c610fd9d9ac0875b80b5eed585586faa3908e945
somyungsub <gkdldy5@naver.com>
2020-05-14 05:52:30 +0900

    Add : JPA 실전2, 지연로딩 성능최적화 2 : Dto 래핑, N+1 문제

--- edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/api/OrderSimpleApiController.java
+++ edu/lecture-jpa-practice1/src/main/java/io/ssosso/jpashop1/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package io.ssosso.jpashop1.api;
 
+import io.ssosso.jpashop1.domain.Address;
 import io.ssosso.jpashop1.domain.Order;
 import io.ssosso.jpashop1.domain.OrderSearch;
+import io.ssosso.jpashop1.domain.OrderStatus;
 import io.ssosso.jpashop1.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 성능 최적화
@@ -44,6 +49,42 @@ public class OrderSimpleApiController {
       order.getDelivery().getAddress(); // 지연로딩 -> 강제 초기화 (select SQL 실행)
     });
     return all;
+  }
+
+  /**
+   * Order -> SQL 1 번
+   * Order.Member -> SQL 멤버 수, 단건조회       (ex 2번)
+   * Order.Delivery -> SQL 딜리버리 수, 단건조회  (ex 2번)
+   * -> 총 5번 sql 실행됨... [N+1 문제] -> 1 + 2 + 2
+   *  -> 1 : orders 실행
+   *  -> N : 연관된 객체의 sql 실행 (지연로딩에 의해)
+   *       : N은 연관된 인스턴스 수만큼 select sql 실행됨 (단건으로 실행되서 그럼..)
+   */
+  @GetMapping("/api/v2/simple-orders")
+  public List<SimpleOrderDto> ordersV2() {
+    final List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+    final List<SimpleOrderDto> collect = orders.stream()
+            .map(SimpleOrderDto::new)
+            .collect(Collectors.toList());
+
+    return collect;
+  }
+
+  @Data
+  static class SimpleOrderDto{
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderDto(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName();           // Lazy 초기화 시점1
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress();   // Lazy 초기화 시점2
+    }
   }
 
 }
