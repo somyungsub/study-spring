06da0f8ea8108d25a90ecec2d095661a2e73aea6
somyungsub <gkdldy5@naver.com>
2020-05-16 19:29:24 +0900

    Add : Spring Data JPA, 프로젝트 생성

---
+++ edu/inflearn-spring-data-jpa/src/main/java/io/ssosso/springdatajpa/LectureSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package io.ssosso.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSpringDataJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LectureSpringDataJpaApplication.class, args);
+  }
+
+}
---
+++ edu/inflearn-spring-data-jpa/src/test/java/io/ssosso/springdatajpa/LectureSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssosso.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureSpringDataJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
