1b35d4b0e9218aeec0af11dd2b3ac1a000f68fd5
somyungsub <gkdldy5@naver.com>
2020-05-18 08:08:00 +0900

    Add : QueryDsl, JPA 설정

--- edu/inflearn-querydsl/src/test/java/io/ssosso/querydsl/QuerydslApplicationTests.java
+++ edu/inflearn-querydsl/src/test/java/io/ssosso/querydsl/QuerydslApplicationTests.java
@@ -6,6 +6,7 @@ import io.ssosso.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
   @Autowired
