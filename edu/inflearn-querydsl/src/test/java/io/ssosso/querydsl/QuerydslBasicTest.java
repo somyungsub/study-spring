cae76d3cc1b8bcd9f1eaa9703509bd2c4b26e0fe
somyungsub <gkdldy5@naver.com>
2020-05-18 21:45:14 +0900

    Add : QueryDsl, 페이징

--- edu/inflearn-querydsl/src/test/java/io/ssosso/querydsl/QuerydslBasicTest.java
+++ edu/inflearn-querydsl/src/test/java/io/ssosso/querydsl/QuerydslBasicTest.java
@@ -189,7 +189,36 @@ public class QuerydslBasicTest {
     assertThat(member5.getUsername()).isEqualTo("member5");
     assertThat(member6.getUsername()).isEqualTo("member6");
     assertThat(memberNull.getUsername()).isNull();
+  }
 
+  @Test
+  @DisplayName("페이징")
+  public void paging() {
+    List<Member> result = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.desc())
+            .offset(1)
+            .limit(2)
+            .fetch();
+
+    assertThat(result.size()).isEqualTo(2);
+  }
+
+  @Test
+  @DisplayName("페이징")
+  public void paging2() {
+
+    QueryResults<Member> results = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.desc())
+            .offset(1)
+            .limit(2)
+            .fetchResults();
+
+    assertThat(results.getTotal()).isEqualTo(4);
+    assertThat(results.getLimit()).isEqualTo(2);
+    assertThat(results.getOffset()).isEqualTo(1);
+    assertThat(results.getResults().size()).isEqualTo(2);
   }
 
 
