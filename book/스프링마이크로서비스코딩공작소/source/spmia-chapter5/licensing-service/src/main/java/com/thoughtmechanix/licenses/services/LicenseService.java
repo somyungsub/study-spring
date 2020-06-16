ed66291b3157b14e6c711e194547a21514d80213
Devso <gkdldy5@gmail.com>
2020-06-16 19:19:58 +0900

    Add : 스프링 마이크로서비스 코딩공작소, 5장 히스트릭스

--- book/스프링마이크로서비스코딩공작소/source/spmia-chapter5/licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
+++ book/스프링마이크로서비스코딩공작소/source/spmia-chapter5/licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -20,93 +20,112 @@ import java.util.UUID;
 
 @Service
 public class LicenseService {
-    private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
-    @Autowired
-    private LicenseRepository licenseRepository;
+  private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
+  @Autowired
+  private LicenseRepository licenseRepository;
 
-    @Autowired
-    ServiceConfig config;
+  @Autowired
+  ServiceConfig config;
 
-    @Autowired
-    OrganizationRestTemplateClient organizationRestClient;
+  @Autowired
+  OrganizationRestTemplateClient organizationRestClient;
 
 
-    public License getLicense(String organizationId,String licenseId) {
-        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+  public License getLicense(String organizationId, String licenseId) {
+    License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
 
-        Organization org = getOrganization(organizationId);
+    Organization org = getOrganization(organizationId);
 
-        return license
-                .withOrganizationName( org.getName())
-                .withContactName( org.getContactName())
-                .withContactEmail( org.getContactEmail() )
-                .withContactPhone( org.getContactPhone() )
-                .withComment(config.getExampleProperty());
+    return license
+        .withOrganizationName(org.getName())
+        .withContactName(org.getContactName())
+        .withContactEmail(org.getContactEmail())
+        .withContactPhone(org.getContactPhone())
+        .withComment(config.getExampleProperty());
+  }
+
+  @HystrixCommand
+  private Organization getOrganization(String organizationId) {
+    return organizationRestClient.getOrganization(organizationId);
+  }
+
+  private void randomlyRunLong() {
+    Random rand = new Random();
+
+    int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+
+    if (randomNum == 3) sleep();
+  }
+
+  private void sleep() {
+    try {
+      Thread.sleep(11000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    @HystrixCommand
-    private Organization getOrganization(String organizationId) {
-        return organizationRestClient.getOrganization(organizationId);
-    }
+//    @HystrixCommand(//fallbackMethod = "buildFallbackLicenseList",
+//            threadPoolKey = "licenseByOrgThreadPool",
+//            threadPoolProperties =
+//                    {@HystrixProperty(name = "coreSize",value="30"),
+//                     @HystrixProperty(name="maxQueueSize", value="10")},
+//            commandProperties={
+//                     @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="10"),
+//                     @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="75"),
+//                     @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="7000"),
+//                     @HystrixProperty(name="metrics.rollingStats.timeInMilliseconds", value="15000")
+//                     @HystrixProperty(name="metrics.rollingStats.numBuckets", value="5")
+//                     }
+//    )
+//    public List<License> getLicensesByOrg(String organizationId){
+//        logger.debug("LicenseService.getLicensesByOrg  Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+//        randomlyRunLong();
+//
+//        return licenseRepository.findByOrganizationId(organizationId);
+//    }
 
-    private void randomlyRunLong(){
-      Random rand = new Random();
+//  @HystrixCommand(
+//      commandProperties =
+//          {
+//              @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "12000")
+//          }
+//  )
+//  public List<License> getLicensesByOrg(String organizationId) {
+//    logger.debug("LicenseService.getLicensesByOrg  Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+//    randomlyRunLong();
+//    return licenseRepository.findByOrganizationId(organizationId);
+//  }
 
-      int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+  @HystrixCommand(fallbackMethod = "buildFallbackLicenseList")
+  public List<License> getLicensesByOrg(String organizationId){
+    randomlyRunLong();
+    return licenseRepository.findByOrganizationId(organizationId);
+  }
 
-      if (randomNum==3) sleep();
-    }
+  private List<License> buildFallbackLicenseList(String organizationId) {
+    List<License> fallbackList = new ArrayList<>();
+    License license = new License()
+        .withId("0000000-00-00000")
+        .withOrganizationId(organizationId)
+        .withProductName("Sorry no licensing information currently available");
 
-    private void sleep(){
-        try {
-            Thread.sleep(11000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    fallbackList.add(license);
+    return fallbackList;
+  }
 
-    @HystrixCommand(//fallbackMethod = "buildFallbackLicenseList",
-            threadPoolKey = "licenseByOrgThreadPool",
-            threadPoolProperties =
-                    {@HystrixProperty(name = "coreSize",value="30"),
-                     @HystrixProperty(name="maxQueueSize", value="10")},
-            commandProperties={
-                     @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="10"),
-                     @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="75"),
-                     @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="7000"),
-                     @HystrixProperty(name="metrics.rollingStats.timeInMilliseconds", value="15000"),
-                     @HystrixProperty(name="metrics.rollingStats.numBuckets", value="5")}
-    )
-    public List<License> getLicensesByOrg(String organizationId){
-        logger.debug("LicenseService.getLicensesByOrg  Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
-        randomlyRunLong();
+  public void saveLicense(License license) {
+    license.withId(UUID.randomUUID().toString());
 
-        return licenseRepository.findByOrganizationId(organizationId);
-    }
+    licenseRepository.save(license);
+  }
 
-    private List<License> buildFallbackLicenseList(String organizationId){
-        List<License> fallbackList = new ArrayList<>();
-        License license = new License()
-                .withId("0000000-00-00000")
-                .withOrganizationId( organizationId )
-                .withProductName("Sorry no licensing information currently available");
+  public void updateLicense(License license) {
+    licenseRepository.save(license);
+  }
 
-        fallbackList.add(license);
-        return fallbackList;
-    }
-
-    public void saveLicense(License license){
-        license.withId( UUID.randomUUID().toString());
-
-        licenseRepository.save(license);
-    }
-
-    public void updateLicense(License license){
-      licenseRepository.save(license);
-    }
-
-    public void deleteLicense(License license){
-        licenseRepository.delete(license);
-    }
+  public void deleteLicense(License license) {
+    licenseRepository.delete(license);
+  }
 
 }
