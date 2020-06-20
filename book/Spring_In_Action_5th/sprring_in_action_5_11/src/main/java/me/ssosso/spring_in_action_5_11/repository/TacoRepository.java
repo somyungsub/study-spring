00c21433ef6b448a47a90022b8b78b8b7dabde26
somyungsub <gkdldy5@naver.com>
2020-06-21 08:37:41 +0900

    Add : Spring in Action 5판, 11장 리액티브 API 개발하기 - 컨트롤러

--- book/Spring_In_Action_5th/sprring_in_action_5_11/src/main/java/me/ssosso/spring_in_action_5_11/repository/TacoRepository.java
+++ book/Spring_In_Action_5th/sprring_in_action_5_11/src/main/java/me/ssosso/spring_in_action_5_11/repository/TacoRepository.java
@@ -1,8 +1,6 @@
 package me.ssosso.spring_in_action_5_11.repository;
 
 import me.ssosso.spring_in_action_5_11.domain.Taco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface TacoRepository extends ReactiveCrudRepository<Taco, Long> {
