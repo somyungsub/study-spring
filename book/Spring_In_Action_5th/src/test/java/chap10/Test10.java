1bc0508a3aeeeeb0f245a7897896fb213b1aaf1b
somyungsub <gkdldy5@naver.com>
2020-06-20 22:44:46 +0900

    Add : Spring in Action 5판, 10장 리액터 개요 - 조합 연산자

--- book/Spring_In_Action_5th/src/test/java/chap10/Test10.java
+++ book/Spring_In_Action_5th/src/test/java/chap10/Test10.java
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
-import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class Test10 {
+
+  private void print(String s) {
+    System.out.println(s);
+  }
+
   @Test
   @DisplayName("just")
   public void p_348(){
@@ -101,7 +105,7 @@ public class Test10 {
 
   @Test
   @DisplayName("create")
-  public void create  () throws InterruptedException {
+  public void create() throws InterruptedException {
     Flux<Object> flux = Flux.create(fluxSink -> {
       fluxSink.next(1L);
       fluxSink.next(2L);
@@ -113,10 +117,74 @@ public class Test10 {
     flux.subscribe(f -> System.out.println("sub : " + f));
 
     Thread.sleep(1000L);
-
   }
 
-  
+  @Test
+  @DisplayName("mergeWith")
+  public void p_353() throws Exception{
+
+    // 시간순으로 방출
+    Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa")
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<String> foodFlux = Flux.just("Lasagna", "Lollipos", "Apples")
+        .delaySubscription(Duration.ofMillis(700L))
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<String> result = characterFlux.mergeWith(foodFlux);
+    result.subscribe(this::print);
+
+    Thread.sleep(3000L);
+  }
+
+  @Test
+  @DisplayName("zip")
+  public void p_354() throws Exception{
+    // 한번씩 번갈아가면서 방출 , 순서유지
+    Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa")
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<String> foodFlux = Flux.just("Lasagna", "Lollipos", "Apples")
+        .delaySubscription(Duration.ofMillis(700L))
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<Tuple2<String, String>> result = Flux.zip(characterFlux, foodFlux);
+    result.subscribe(objects -> System.out.println(objects.getT1() + ", " + objects.getT2()));
+
+    Thread.sleep(3000L);
+  }
+
+  @Test
+  @DisplayName("zip2")
+  public void p_354_2() throws Exception{
+
+    Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa")
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<String> foodFlux = Flux.just("Lasagna", "Lollipos", "Apples")
+        .delaySubscription(Duration.ofMillis(700L))
+        .delayElements(Duration.ofMillis(500));
+
+    // 반환값 변경 (map과 유사)
+    Flux<String> result = Flux.zip(characterFlux, foodFlux, (s, s2) -> s + " eats " + s2);
+    result.subscribe(this::print);
+
+    Thread.sleep(3000L);
+  }
+
+  @Test
+  @DisplayName("first")
+  public void p_356() throws Exception{
+    // 둘중에 먼저 통지 되는 flux를 선택하여 통지
+    Flux<String> slowFlux = Flux.just("tortoise", "snail", "sloth")
+        .delaySubscription(Duration.ofMillis(100));
+    Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+
+    Flux<String> firstFlux = Flux.first(slowFlux, fastFlux);
+
+    firstFlux.subscribe(this::print);
+  }
+
 
 
 }
