d601103cd600836480e74aba581b4c867f81cebc
Devso <gkdldy5@gmail.com>
2020-07-30 18:00:37 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), security 추가

---
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityController.java
@@ -0,0 +1,13 @@
+package me.ssosso.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+  @GetMapping("/")
+  public String index() {
+    return "home";
+  }
+}
