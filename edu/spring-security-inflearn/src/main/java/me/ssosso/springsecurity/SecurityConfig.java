b3a40a8e8d65ada4a0a46b7b87dd3bb373b8ca98
Devso <gkdldy5@gmail.com>
2020-07-31 15:44:46 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), 인증

---
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfig.java
@@ -0,0 +1,30 @@
+package me.ssosso.springsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity  // 웹시큐리티 설정 클래스들 임포트 시키는 애노테이션이므로 필수로 선언해야함
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  // http 요청에 대한 시큐리티 설정
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    // 인가 정책
+    http
+        .authorizeRequests()
+        .anyRequest().authenticated();
+
+
+    // 인증 정책
+    http
+        .formLogin()
+
+
+
+    ;
+  }
+}
