66a18ba4c884775193fbeb913a8eb00f24a6e8ef
somyungsub <gkdldy5@naver.com>
2020-08-02 14:03:58 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), Logout 처리

--- edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfig.java
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -51,8 +54,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             httpServletResponse.sendRedirect("/login");
           }
         })
-        .permitAll()  // 모든 사용자 접근 가능함 로그인 URL은
+        .permitAll();  // 모든 사용자 접근 가능함 로그인 URL은
+
+
+    // 로그아웃
+    http
+        .logout()
+        .logoutUrl("/logout") // 디폴트 post 방식, get은 에러 -> 설정 필요
+        .logoutSuccessUrl("/logiin")
+        .addLogoutHandler(new LogoutHandler() {
+          @Override
+          public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+            HttpSession session = httpServletRequest.getSession();
+            session.invalidate();
+          }
+        })
+        .logoutSuccessHandler(new LogoutSuccessHandler() {
+          @Override
+          public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+            httpServletResponse.sendRedirect("/login");
+          }
+        })
+        .deleteCookies("remember-me") // 쿠키 삭제
     ;
+
 
   }
 }
