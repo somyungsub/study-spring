e37d2212bd0674b95b0f28b8c1d3e9866efd2b6e
somyungsub <gkdldy5@naver.com>
2020-08-07 17:19:01 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), 예외 처리 이해

--- edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfigAuthorize.java
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfigAuthorize.java
@@ -1,10 +1,24 @@
 package me.ssosso.springsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity  // 웹시큐리티 설정 클래스들 임포트 시키는 애노테이션이므로 필수로 선언해야함
@@ -33,6 +47,7 @@ public class SecurityConfigAuthorize extends WebSecurityConfigurerAdapter {
     // 인가 정책
     http
         .authorizeRequests()  // 모든 요청
+        .antMatchers("/login").permitAll()
         .antMatchers("/user").hasRole("USER") // /user 요청 -> USER 권한 필요
         .antMatchers("/admin/pay").hasRole("ADMIN")
         .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
@@ -41,7 +56,36 @@ public class SecurityConfigAuthorize extends WebSecurityConfigurerAdapter {
 
     // 인증 정책
     http
-        .formLogin();
+        .formLogin()
+        .successHandler(new AuthenticationSuccessHandler() {
+          @Override
+          public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+            // 캐시 저장된 정보 재사용
+            RequestCache requestCache = new HttpSessionRequestCache();
+            SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, httpServletResponse);
+            String redirectUrl = savedRequest.getRedirectUrl();
+            httpServletResponse.sendRedirect(redirectUrl);
+          }
+        })
+    ;
+
+    // 얘외처리
+    http
+        .exceptionHandling()
+//        .authenticationEntryPoint(new AuthenticationEntryPoint() {
+//          @Override
+//          public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+//            httpServletResponse.sendRedirect("/login"); // 사용자 정의 /login로 매핑됨
+//          }
+//        })
+        .accessDeniedHandler(new AccessDeniedHandler() {
+          @Override
+          public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+            httpServletResponse.sendRedirect("/denied");
+          }
+        })
+
+        ;
 
 
 
--- edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityController.java
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityController.java
@@ -30,4 +30,15 @@ public class SecurityController {
   public String admin(){
     return "admin";
   }
+
+  @GetMapping("/denied")
+  public String denied(){
+    return "denied";
+  }
+
+  @GetMapping("/login")
+  public String login(){
+    return "login";
+  }
+
 }
