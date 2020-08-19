adf7586656ba295308997f6859f54faea492b37e
Devso <gkdldy5@gmail.com>
2020-08-19 11:22:29 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), SecurityContextHolder, SecurityContext

--- edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityController.java
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityController.java
@@ -1,14 +1,41 @@
 package me.ssosso.springsecurity;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
   @GetMapping("/")
-  public String index() {
+  public String index(HttpSession httpSession) {
+
+    // 저장된 인증 객체
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    // HttpSession을 이용해서 인증 객체 받을 수 있음
+    SecurityContext securityContext = (SecurityContext)httpSession.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+    Authentication authentication2 = securityContext.getAuthentication();
+
     return "home";
+  }
+
+  @GetMapping("/thread")
+  public String thread() {
+    new Thread(
+        () -> {
+          Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+          System.out.println("authentication = " + authentication);
+        }
+    ).start();
+
+    return "thread";
   }
 
   @GetMapping("loginPage")
