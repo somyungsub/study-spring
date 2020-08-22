297bf5dfc2d9bcb8d2cee096766885360e8e3bf6
somyungsub <gkdldy5@gmail.com>
2020-08-22 23:29:49 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), AccessDecision Manager/Voter

--- edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfigAuthorization.java
+++ edu/spring-security-inflearn/src/main/java/me/ssosso/springsecurity/SecurityConfigAuthorization.java
@@ -5,15 +5,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfigManager extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigAuthorization extends WebSecurityConfigurerAdapter {
+
+  /*
+    인가처리
+    - AccessDecisionManager
+    - AccessDecisionVoter 3 가지 유형
+   */
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .anyRequest().authenticated();
+        .antMatchers("/user").hasRole("USER")
+        .anyRequest().permitAll();
 
     http
         .formLogin();
