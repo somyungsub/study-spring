cd4efc5bf1f981b5b45f3af22f8dee51d01f719b
Devso <gkdldy5@gmail.com>
2020-08-24 16:56:42 +0900

    Add : 스프링시큐리티 - 정수원 (인프런강의), 실전 - 커스텀 logout

--- edu/spring-security-inflearn/spring-security-practice1/src/main/java/io/ssosso/springsecuritypractice1/controller/login/LoginController.java
+++ edu/spring-security-inflearn/spring-security-practice1/src/main/java/io/ssosso/springsecuritypractice1/controller/login/LoginController.java
@@ -1,14 +1,31 @@
 package io.ssosso.springsecuritypractice1.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class LoginController {
-  
+
   @GetMapping("/login")
-  public String login(){
-    
+  public String login() {
     return "login";
+  }
+
+  @GetMapping("/logout")
+  public String logout(HttpServletRequest request, HttpServletResponse response) {
+    // 저장된 인증객체 -> 얻기
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null) {
+      // 로그아웃처리 -> 내부적으로 clearContext()실행 됨 -> 세션에서 제거 시킴
+      new SecurityContextLogoutHandler().logout(request, response, authentication);
+    }
+    return "redirect:/login";
   }
 }
